0683c698cdddb5c4ffde24e1d2a56a3c548c408d
INDavid04 <davidirimia04@gmail.com>
2024-12-20 19:41:35 +0200

    formulare partea 1 20dec24

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,31 @@
+function populateStorage() {
+   localStorage.setItem("bgcolor", document.getElementById("culoare").value);
+   setStyle();
+}
 
-window.onload = function(){
-  
-    /* Adăugați cod pentru schimbarea culorii de fundal și 
-       pentru eticheta cu valoarea creditului social      */
-       
-    /* ... */
-  
-  }
+function setStyle() {
+   const currentColor = localStorage.getItem("bgcolor");
+   document.getElementById("culoare").value = currentColor;
+   document.body.style.backgroundColor = currentColor;
+}
+
+window.onload = function() {  
+   /* Adăugați cod pentru schimbarea culorii de fundal și pentru eticheta cu valoarea creditului social */
+   
+   // Eticheta cu valoarea creditului social
+   const value = document.querySelector("#value");
+   const input = document.querySelector("#credit");
+   value.textContent = input.value;
+   input.addEventListener("input", function(event) {
+      value.textContent = event.target.value;
+   })
+
+   // schimbarea culorii de fundal
+   if (!localStorage.getItem("bgcolor")) {
+      populateStorage();
+   } else {
+      setStyle();
+   }
+   const bgColorForm = document.getElementById("culoare");
+   bgColorForm.onchange = populateStorage;
+}
